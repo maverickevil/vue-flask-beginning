2192e5a38807985c16c659ef6748e29ad5679ce8
Mystic <58482090+PokeyBoa@users.noreply.github.com>
2022-09-23 13:26:34 +0800

    Feat: 前端增加跨域请求配置

--- web-vue/vue.config.js
+++ web-vue/vue.config.js
@@ -13,11 +13,15 @@ module.exports = defineConfig({
     host: '127.0.0.1',
     port: 8000,
     https: false,
+
+    // 前端解决跨域问题
+    // 提醒：在ajax请求或<a>标签href属性中，host url要写前端的端口号（注意不是后端）
     // proxy: {
     //   // 配置跨域
     //   '/api': {
-    //     target: 'http://127.0.0.1:5000/',     // 这里填写项目真实的后台接口地址
-    //     changOrigin: true,                    // 设置允许跨域
+    //     target: 'http://127.0.0.1:5000',         // 这里填写项目真实的后台接口地址
+    //     changOrigin: true,                       // 设置允许跨域
+    //     ws: true,
     //     // pathRewrite: {                        // 这个重写不可省略！因为我们真正请求的地址并不含 /api
     //     //   '^/api': ''
     //     //     /* 
